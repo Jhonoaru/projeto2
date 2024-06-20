cd29311b2de059b161f975998714474304d29a77
Jhonoaru <joao.victor462002@gmail.com>
2024-06-20 00:37:21 -0300

    Update
    Teste

--- scripts/criarUsuario.js
+++ scripts/criarUsuario.js
@@ -1,6 +1,8 @@
 document.getElementById("formulario").addEventListener('submit', function(event){
     event.preventDefault();
 
+    document.getElementById('excluir-todos').addEventListener('click', excluirTodosUsuarios);
+
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
 
@@ -48,6 +50,44 @@ function excluirUsuario(id) {
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
     atualizarListaUsuarios();
 }
+
+function excluirTodosUsuarios() {
+    localStorage.removeItem('usuarios');
+    atualizarListaUsuarios();
+}
+
+document.getElementById('mostrar-usuario').addEventListener('click', function() {
+    const listaUsuariosDiv = document.querySelector('.listaUsuario');
+    if(listaUsuariosDiv.style.display === 'none' || listaUsuariosDiv.style.display === ''){
+        listaUsuariosDiv.style.display = 'block';
+    } else {
+        listaUsuariosDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('mostrar-pesquisa').addEventListener('click', function() {
+    const campoPesquisa = document.getElementById('campo-pesquisa');
+    if(campoPesquisa.style.display === 'none' || campoPesquisa.style.display === ''){
+        campoPesquisa.style.display = 'block';
+    } else {
+        campoPesquisa.style.display = 'none';
+    }
+});
+
+document.getElementById('botao-pesquisar').addEventListener('click', function() {
+    const pesquisa = document.getElementById('pesquisa').value.toLowerCase();
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const usuarioEncontrado = usuarios.find(usuario => usuario.email.toLowerCase().includes(pesquisa) || usuario.id.toString().includes(pesquisa));
+
+    const resultadoPesquisa = document.getElementById('resultado-pesquisa');
+    const usuarioEncontradoElemento = document.getElementById('usuario-encontrado');
+    if (usuarioEncontrado) {
+        usuarioEncontradoElemento.textContent = `ID: ${usuarioEncontrado.id}, Nome: ${usuarioEncontrado.nome}, E-mail: ${usuarioEncontrado.email}, Data de Cadastro: ${usuarioEncontrado.dataCadastro}`;
+    } else {
+        usuarioEncontradoElemento.textContent = 'Usuário não encontrado';
+    }
+    resultadoPesquisa.style.display = 'block';
+});
 
 // Atualiza a lista de usuários quando a página é carregada
 document.addEventListener('DOMContentLoaded', atualizarListaUsuarios);
